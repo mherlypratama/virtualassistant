44c5e5e586e5fdc92d64b3aeb9d4c5594a7d1d68
mherlypratama <mherlypratama2033@students.unila.ac.id>
2023-06-13 10:27:58 +0700

    update

--- jadi/jadi.py
+++ jadi/jadi.py
@@ -9,8 +9,10 @@ from bs4 import BeautifulSoup
 import pywhatkit
 
 
-openai.api_key = "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"
+# openai.api_key = "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"
+openai.api_key = "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"
 pygame.init()
+
 
 # fungsi untuk mendapatkan hari dan tanggal
 def get_date():
@@ -19,17 +21,19 @@ def get_date():
     date = soup.select_one("#ctdat").text
     return date
 
+
 # fungsi untuk mendapatkan berita terkini dari BBC News
 def get_news():
     url = "https://www.detik.com/"
     res = requests.get(url)
     soup = BeautifulSoup(res.text, "html.parser")
-    news = [i.text.strip() for i in soup.select("h2", class_='title')]
+    news = [i.text.strip() for i in soup.select("h2", class_="title")]
     return news
 
-#Untuk Berbicara
+
+# Untuk Berbicara
 def speak(response_str):
-    tts = gTTS(text=response_str, lang='id', slow=False)
+    tts = gTTS(text=response_str, lang="id", slow=False)
     # Membuat file audio dalam memori
     fp = BytesIO()
     tts.write_to_fp(fp)
@@ -43,7 +47,7 @@ def speak(response_str):
         continue
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     r = sr.Recognizer()
     mic = sr.Microphone(device_index=1)
 
@@ -59,11 +63,11 @@ if __name__ == '__main__':
         print("no longer listening")
 
         try:
-            user_input = r.recognize_google(audio, language='id-ID')
+            user_input = r.recognize_google(audio, language="id-ID")
         except:
             continue
 
-        prompt = user_name+":"+user_input + "\n"+bot_name+":"
+        prompt = user_name + ":" + user_input + "\n" + bot_name + ":"
         conversation += prompt
 
         response = openai.Completion.create(
@@ -73,18 +77,20 @@ if __name__ == '__main__':
             max_tokens=256,
             top_p=1,
             frequency_penalty=0,
-            presence_penalty=0
+            presence_penalty=0,
         )
         print(user_input)
         if "Halo Ana" in user_input or "halo Ana" in user_input:
-            user_input = user_input.replace('Ana','').replace('Halo','').replace('halo','')
-        
+            user_input = (
+                user_input.replace("Ana", "").replace("Halo", "").replace("halo", "")
+            )
+
             if user_input == "selesai":
                 response_str = "Terima kasih telah menggunakan layanan saya"
                 speak(response_str)
                 break
 
-            # TANGGAL 
+            # TANGGAL
             elif "tanggal" in user_input or "hari" in user_input:
                 response_str = "Hari ini adalah " + get_date()
                 speak(response_str)
@@ -92,29 +98,30 @@ if __name__ == '__main__':
             # LAGU
             elif "lagu" in user_input:
                 print(user_input)
-                song = user_input.replace('lagu','')
+                song = user_input.replace("lagu", "")
                 response_str = "Memutar Lagu " + song
 
                 speak(response_str)
                 pywhatkit.playonyt(song)
 
-            #BERITA    
+            # BERITA
             elif "berita" in user_input:
                 news = get_news()
                 response_str = "Berikut adalah beberapa berita terkini dari Detik.com: "
                 speak(response_str)
                 # speak("Berikut adalah beberapa berita terkini dari Detik.com: ")
                 for i, n in enumerate(news[:5]):
-                    response_str = str(i+1) + ". " + n
+                    response_str = str(i + 1) + ". " + n
                     speak(response_str)
             else:
                 response_str = response["choices"][0]["text"].replace("\n", "")
-                response_str =response_str.split(
-                    user_name + ":" ,1)[0].split(bot_name+ ":",1)[0]
+                response_str = response_str.split(user_name + ":", 1)[0].split(
+                    bot_name + ":", 1
+                )[0]
 
-                conversation+= response_str +"\n"
+                conversation += response_str + "\n"
                 print(response_str)
                 speak(response_str)
         else:
-            user_input= ""
+            user_input = ""
             continue
